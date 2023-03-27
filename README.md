d66fc839f4d1fa721eb52d6fbbb0c66491d5c03c
Mike Collingwood <mike.collingwood@softwire.com>
2023-03-27 11:35:46 +0000

    ACADEMY22-481 - Add TableTennisTable-C#

---
+++ README.md
@@ -0,0 +1,50 @@
+# Table Tennis Table - C#
+
+1. Fork this repo
+
+   - Click `Fork` in the top-right of the page - this will create a copy of this repo in **your own GitHub account**
+
+2. Clone (download) the repo
+
+   - Go to your newly-created fork of the repo (on GitHub).
+   - Click `Clone or download` (the green button on the right).
+   - Make sure the page says `Clone with SSH` (rather than `Clone with HTTPS`).
+   - Open your git client (e.g. GitKraken) and use this link to clone the repo.  
+     Your trainer will be able to help you with this.
+
+3. "Cloning the repo" will create a folder on your computer with the files from this repo.  
+   Open this folder in IntelliJ / Visual Studio Code.
+
+4. Open a command-prompt in the project folder, or open the folder in an IDE such as JetBrains Rider, Visual Studio,
+   or VSCode.  
+   Your trainer can show you how to do this, if you need any help.
+
+5. Change directory to "TableTennisTable-CSharp", and run this command to run your code:  
+   `dotnet run`
+   or run the project in your IDE (CTRL+F5 or ⌘+F5 usually)
+
+6. Run this command to test your code:  
+   `dotnet test`
+   or run the tests from your IDE
+
+## Commands
+
+When the app is running the following commands can be run:
+
+- `add player <name>`- adds a player of that name.
+  Example: `add player Alice`
+
+- `print`- prints the state of the league
+
+- `record win <winner> <loser> `- records the result of a match.
+  Example: `record win Alice Mike`
+
+- `winner`- finds the current winner
+
+- `save <filepath>` - saves the current league to a JSON file.
+  Example: `save my_league.json`
+
+- `load <filepath>` - loads a saved league from a JSON file.
+  Example: `load my_league.json`
+
+- `quit` - quits the game
---
+++ TableTennisTable-CSharp/App.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace TableTennisTable_CSharp
+{
+    public class App
+    {
+        private League _league;
+        private ILeagueRenderer _leagueRenderer;
+        private IFileService _fileService;
+
+        public App(League initialLeague, ILeagueRenderer leagueRenderer, IFileService fileService)
+        {
+            _league = initialLeague;
+            _leagueRenderer = leagueRenderer;
+            _fileService = fileService;
+        }
+
+        public string SendCommand(string command)
+        {
+            try
+            {
+                if (command.StartsWith("add player"))
+                {
+                    string playerName = command.Substring(11);
+                    _league.AddPlayer(playerName);
+                    return $"Added player {playerName}";
+                }
+
+                if (command.StartsWith("record win"))
+                {
+                    string playersString = command.Substring(11);
+                    var players = playersString.Split(' ');
+                    string winner = players[0];
+                    string loser = players[1];
+                    _league.RecordWin(winner, loser);
+                    return $"Recorded {winner} win against {loser}";
+                }
+
+                if (command == "print")
+                {
+                    return _leagueRenderer.Render(_league);
+                }
+
+                if (command == "winner")
+                {
+                    return _league.GetWinner();
+                }
+
+                if (command.StartsWith("save"))
+                {
+                    var filePath = command.Substring(5);
+                    _fileService.Save(filePath, _league);
+                    return $"Saved {filePath}";
+                }
+
+                if (command.StartsWith("load"))
+                {
+                    var filePath = command.Substring(5);
+                    _league = _fileService.Load(filePath);
+                    return $"Loaded {filePath}";
+                }
+
+                return $"Unknown command: {command}";
+            }
+            catch (ArgumentException e)
+            {
+                return e.Message;
+            }
+        }
+    }
+}
