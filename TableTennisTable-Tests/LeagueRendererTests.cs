76856b5f57d741026665a09d3ba2fc3a3d2595f4
MoeyCC <mohan.ratnam@cornwall.gov.uk>
2024-03-14 17:18:03 +0000

    Unit tests added for LeagueRenderer

---
+++ TableTennisTable-Tests/LeagueRendererTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+using System.Runtime.CompilerServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TableTennisTable_CSharp;
+
+namespace TableTennisTable_Tests
+{
+    [TestClass]
+    public class LeagueRendererTests
+    {
+        [TestMethod]
+        public void GivenPlayersExists_WhenUserTypesPrint_ThenLeaguePrinted()
+        {
+            // Arrange
+            League league = new League();
+            var leagueRenderer = new LeagueRenderer();
+            var fileService = new FileService();
+            var game = new App(league, leagueRenderer, fileService);
+            league.AddPlayer("Megan");
+            league.AddPlayer("Lizzie");
+            league.AddPlayer("Mia");
+            league.AddPlayer("Nat");
+            league.AddPlayer("Blake");
+            league.AddPlayer("Mohan");
+            var expected = @"                              -------------------
+                              |      Megan      |
+                              -------------------
+                    ------------------- -------------------
+                    |     Lizzie      | |       Mia       |
+                    ------------------- -------------------
+          ------------------- ------------------- -------------------
+          |       Nat       | |      Blake      | |      Mohan      |
+          ------------------- ------------------- -------------------";
+
+            // Act
+            var result = game.SendCommand("print");
+
+            // Assert
+            Assert.AreEqual( expected, result);            
+        }
+
+        [TestMethod]
+        public void GivenNoPlayersExists_WhenUserTypesPrint_ThenMessagePrinted()
+        {
+            // Arrange
+            League league = new League();
+            var leagueRenderer = new LeagueRenderer();
+            var fileService = new FileService();
+            var game = new App(league, leagueRenderer, fileService);
+            
+            // Act
+            var result = game.SendCommand("print");
+
+            // Assert
+            Assert.AreEqual( "No players yet", result);            
+        }        
+    }
+}
